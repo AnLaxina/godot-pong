bf192160aff98e4b24ba2d5cd2b37d133156cb09
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2023-12-06 14:35:02 -0800

    Add working player 2

---
+++ Game/Scripts/player_2.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+# Identical to player 1's script. Just changing how the input is recieved.
+
+@export var paddle_speed : int = 300
+
+func _process(_delta):
+	# Gets the direction based on what the user pressed
+	# If it's 'W' it returns -1 as y in the up direction is -1
+	var direction : float = Input.get_axis("ui_up", "ui_down")
+	
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = paddle_speed * direction
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = paddle_speed * direction
+	
+	# Handle if no button is pressed
+	if direction == 0:
+		velocity.y = 0
+		
+	move_and_slide()
