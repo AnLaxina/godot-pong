1577c2b8f73c9239a5c9b2b80e0f8e619ab341c6
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2023-12-09 17:10:23 -0800

    Adjust ball movement to be more dynamic

--- Game/Scripts/level.gd
+++ Game/Scripts/level.gd
@@ -21,6 +21,26 @@ func _on_right_bound_entered():
 	reset_ball()
 	
 	
-# Resets the ball to its initial position, in the inspector it's 412, 264
+
 func reset_ball():
+	# Resets the ball to its initial position, in the inspector it's 412, 264
+	# Also randomizes the ball's speed and direction for a more "exciting" game
 	$Ball.position = Vector2(412, 264)
+	var ball_speed : int = randi_range(700, 850)
+	
+	# Use a "switch statement" to randomize the direction of the ball diagonally
+	var ball_direction : Vector2 = Vector2.ZERO
+	var random_int : int = randi_range(0,3)
+	match random_int:
+		0:
+			ball_direction = Vector2.RIGHT
+		1:
+			ball_direction = Vector2.LEFT
+		2:
+			ball_direction = Vector2.DOWN
+		3:
+			ball_direction = Vector2.UP
+	var ball_rotation = ball_direction.rotated(randf_range(PI/3, PI/4))
+	
+	# Finally apply the velocity based off the ball's speed and rotation/direction
+	$Ball.velocity = ball_speed * ball_rotation
